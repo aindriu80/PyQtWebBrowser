6a34ef1d44b0bca22105108f7e6998baf46cab21
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-03-31 10:12:17 +0100

    QTabWidget - initial browser setup

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import json
+
+
+
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
+                            QPushButton, QLabel, QLineEdit, QTabBar,
+                            QFrame, QStackedLayout)
+
+from PyQt5.QtGui import QIcon, QWindow, QImage
+from PyQt5.QtCore import *
+
+class App(QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Web Browser")
+        self.setBaseSize(1920, 1200)
+        self.CreateApp()
+
+    def CreateApp(self):
+        self.layout = QVBoxLayout()
+
+        self.tabbar = QTabBar()
+        
+
+        self.tabbar.addTab("Tab 1")
+        self.tabbar.addTab("Tab 2")
+
+        self.tabbar.setCurrentIndex(0)
+
+        self.layout.addWidget(self.tabbar)
+        self.setLayout(self.layout)
+
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = App()
+
+    sys.exit(app.exec_())
