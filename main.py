16d6e5e96f6b03a7ae117ee41d6b0fa55e587002
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-04-01 10:20:44 +0100

    Interacting with the Tabs

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import sys
 import os
 import json
 
-
-
 from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
                             QPushButton, QLabel, QLineEdit, QTabBar,
-                            QFrame, QStackedLayout)
+                            QFrame, QStackedLayout, QTabWidget)
 
 from PyQt5.QtGui import QIcon, QWindow, QImage
 from PyQt5.QtCore import *
+
+class AddressBar(QLineEdit):
+    def __init__(self):
+        super().__init__()
+
+    def mousePressEvent(self, e):
+        self.selectAll()
 
 class App(QFrame):
     def __init__(self):
@@ -20,19 +25,38 @@ class App(QFrame):
 
     def CreateApp(self):
         self.layout = QVBoxLayout()
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0,0,0,0)
 
-        self.tabbar = QTabBar()
-        
+        # Create Tabs
+        self.tabbar = QTabBar(movable=True, tabsClosable=True)
+        self.tabbar.tabCloseRequested.connect(self.CloseTab)
+
 
         self.tabbar.addTab("Tab 1")
         self.tabbar.addTab("Tab 2")
 
         self.tabbar.setCurrentIndex(0)
 
+        # Create AddressBar
+        self.Toolbar = QWidget()
+        self.ToolbarLayout = QHBoxLayout()
+        self.addressbar = AddressBar()
+
+        self.Toolbar.setLayout(self.ToolbarLayout)
+        self.ToolbarLayout.addWidget(self.addressbar)
+
         self.layout.addWidget(self.tabbar)
+        self.layout.addWidget(self.Toolbar)
+
         self.setLayout(self.layout)
 
         self.show()
+
+    def CloseTab(self, i):
+        self.tabbar.removeTab(i)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
