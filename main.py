8f1019f8c3ef7d6b672f695cfc290f99395455a8
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-04-02 09:56:17 +0100

    QWebEngine Implementation - homepage  working

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
 
 from PyQt5.QtGui import QIcon, QWindow, QImage
 from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
 
 class AddressBar(QLineEdit):
     def __init__(self):
@@ -32,11 +33,11 @@ class App(QFrame):
         self.tabbar = QTabBar(movable=True, tabsClosable=True)
         self.tabbar.tabCloseRequested.connect(self.CloseTab)
 
-
-        self.tabbar.addTab("Tab 1")
-        self.tabbar.addTab("Tab 2")
-
         self.tabbar.setCurrentIndex(0)
+
+        # Keep track of the tabs
+        self.tabCount = 0
+        self.tabs = []
 
         # Create AddressBar
         self.Toolbar = QWidget()
@@ -46,17 +47,56 @@ class App(QFrame):
         self.Toolbar.setLayout(self.ToolbarLayout)
         self.ToolbarLayout.addWidget(self.addressbar)
 
+        # New tab button
+        self.AddTabButton = QPushButton("+")
+        self.AddTabButton.clicked.connect(self.AddTab)
+
+        self.ToolbarLayout.addWidget(self.AddTabButton)
+
+        # set main view
+        self.container = QWidget()
+        self.container.layout = QStackedLayout()
+        self.container.setLayout(self.container.layout)
+
         self.layout.addWidget(self.tabbar)
         self.layout.addWidget(self.Toolbar)
+        self.layout.addWidget(self.container)
 
         self.setLayout(self.layout)
+
+        self.AddTab()
 
         self.show()
 
     def CloseTab(self, i):
         self.tabbar.removeTab(i)
 
+    def AddTab(self):
+        i = self.tabCount
 
+        self.tabs.append(QWidget())
+        self.tabs[i].layout = QVBoxLayout()
+        self.tabs[i].setObjectName("tab" + str(i))
+
+        # Open webview
+        self.tabs[i].content = QWebEngineView()
+        self.tabs[i].content.load(QUrl.fromUserInput("https://www.google.ie/"))
+
+        # Add webview to tabs layout
+        self.tabs[i].layout.addWidget(self.tabs[i].content)
+
+        # set top level tab from [] to layout
+        self.tabs[i].setLayout(self.tabs[i].layout)
+
+        # Add tabe to top level stackwidget
+        self.container.layout.addWidget(self.tabs[i])
+        self.container.layout.setCurrentWidget(self.tabs[i])
+
+        # Set the tab at the top of the screen
+        self.tabbar.addTab("New Tab")
+        self.tabbar.setTabData(i,"tab" + str(i))
+        self.tabbar.setCurrentIndex(i)
+        pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
